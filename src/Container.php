a6376555ecdc8f7f0e0557966485e46afda29dfa
Patrick Blom <p.blom@shopmacher.de>
2016-10-20 08:18:05 +0200

    - initial commit

---
+++ src/Container.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace OxErpTest;
+
+use OxErpTest\Container\CompilerPass\CommandCollectorCompilerPass;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class Container
+{
+    /**
+     * @var ContainerInterface;
+     */
+    private static $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public static function Instance()
+    {
+        if (static::$instance === null) {
+            static::$instance = self::initializeContainer();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * @return ContainerBuilder
+     */
+    public static function initializeContainer()
+    {
+        $container = new ContainerBuilder();
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+        $loader->load('services.xml');
+
+        $container->addCompilerPass(new CommandCollectorCompilerPass());
+        $container->compile();
+
+        return $container;
+    }
+
+    private function __clone()
+    {
+    }
+}
---
+++ src/Container/CompilerPass/CommandCollectorCompilerPass.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OxErpTest\Container\CompilerPass;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class CommandCollectorCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        foreach ($container->findTaggedServiceIds('oxerptest.command') as $id => $attr) {
+
+            $definition = $container->getDefinition($id);
+            if (!$definition->isPublic()) {
+                throw new \InvalidArgumentException(sprintf('Command "%s" must be public.', $id));
+            }
+
+            $command = $container->get($id);
+            if ($command instanceof Command) {
+                $application = $container->get('oxerptest.application');
+                $application->add($command);
+            }
+        }
+    }
+}
