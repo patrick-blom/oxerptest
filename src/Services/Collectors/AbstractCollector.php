1e90884210ee57ad8b7d81e2d5db59fdd137b46d
patrickblom <blomsmail@gmail.com>
2016-11-02 21:01:34 +0100

    - editing abstract class

--- src/Services/Collectors/AbstractCollector.php
+++ src/Services/Collectors/AbstractCollector.php
@@ -2,9 +2,7 @@
 
 namespace OxErpTest\Services\Collectors;
 
-use Symfony\Component\Config\Definition\Exception\Exception;
-
-class AbstractCollector
+abstract class AbstractCollector
 {
     const VAR_PATH = __DIR__ . '/../../../var';
     const CALL_PATH = self::VAR_PATH . '/calls';
@@ -12,19 +10,20 @@ class AbstractCollector
 
     /**
      * @return bool
+     * @throws \Exception
      */
     protected function ensurePath()
     {
         if (!is_dir(self::VAR_PATH)) {
-            throw new Exception('could not find ./var directory plz create it');
+            throw new \Exception('could not find ./var directory plz create it');
         }
 
         if (!is_dir(self::CALL_PATH)) {
-            throw new Exception('could not find ./var/calls directory plz create it, and add some calls');
+            throw new \Exception('could not find ./var/calls directory plz create it, and add some calls');
         }
 
         if (!is_dir(self::RESPONSES_PATH)) {
-            throw new Exception('could not find ./var/responses directory plz create it, and add some responses');
+            throw new \Exception('could not find ./var/responses directory plz create it, and add some responses');
         }
 
         return true;
